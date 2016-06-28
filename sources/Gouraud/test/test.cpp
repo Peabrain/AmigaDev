a45c1c8bfadc10e267df8fe7a9d65a70221189bd
Andreas <andreas@Peabrains-MacBook-Pro.local>
2016-06-28 19:46:25 +0200

    f

--- sources/Gouraud/test/test.cpp
+++ sources/Gouraud/test/test.cpp
@@ -4,10 +4,14 @@
 #define	PI 3.141592653589793
 #define	SINCOS 1024
 //////////////////////////////////////////////////////
-typedef struct VEC
+typedef struct VEC3
 {
 	int	x,y,z;	
-}VEC;
+}VEC3;
+typedef struct VEC2
+{
+	int	x,y;	
+}VEC2;
 typedef struct EDGE
 {
 	int a,b;	
@@ -16,10 +20,10 @@ typedef	struct POLYGON
 {
 	int	vertices[4];	// -1 = means no vector
 	int edges[4];		// -1 = means no edge
-	int Cos;			// Gouraudfactor
+	int dot;			// Gouraudfactor
 }POLYGON;
 //////////////////////////////////////////////////////
-VEC	VectorsOrg[8] = 
+VEC3	VectorsOrg[8] = 
 {
 	{-256,-256,-256},	// 0
 	{ 256,-256,-256},	// 1
@@ -54,14 +58,15 @@ POLYGON	Polygons[] =
 	{{7,6,1,0},{6,9,0,8}},
 	{{3,2,5,4},{2,10,4,11}}
 };
-VEC Vectors[8];
+VEC3 Vectors[8];
+VEC2 Vectors2D[8];
 int RotationMatrix[] = 
 {
 	0,0,0,0,
 	0,0,0,0,
 	0,0,0,0,
 };
-int WinkelA = (128) & (SINCOS - 1),WinkelB = (0) & (SINCOS - 1),WinkelC = (0) & (SINCOS - 1);
+int WinkelX = (-128) & (SINCOS - 1),WinkelY = (0) & (SINCOS - 1),WinkelZ = (128) & (SINCOS - 1);
 //////////////////////////////////////////////////////
 int	Sin[SINCOS];
 int	Cos[SINCOS];
@@ -70,16 +75,16 @@ int EdgeVisible[12];
 void	Init();
 void	CalculateVectors();
 void	PrintVectors();
-int	Dot(VEC v0,VEC v1);
-VEC 	Cross(VEC v0,VEC v1);
-bool	IsPolygonVisible(int p,VEC source);
-void	CheckPolygonsVisible(VEC source);
+int	Dot(VEC3 v0,VEC3 v1);
+VEC3 	Cross(VEC3 v0,VEC3 v1);
+bool	IsPolygonVisible(int p,VEC3 source);
+void	CheckPolygonsVisible(VEC3 source);
 void	CalculateRotationMatrix();
-VEC 	CalculateVector(VEC v);
+VEC3 	CalculateVector(VEC3 v);
 //////////////////////////////////////////////////////
 int	main()
 {
-	VEC source = {0,0,0};
+	VEC3 source = {0,0,0};
 	Init();
 	CalculateRotationMatrix();
 	CalculateVectors();	
@@ -99,8 +104,12 @@ void	Init()
 //////////////////////////////////////////////////////
 void	CalculateVectors()
 {
-	for(int i = 0;i < sizeof(VectorsOrg)/sizeof(VEC);i++)
+	for(int i = 0;i < sizeof(VectorsOrg)/sizeof(VEC3);i++)
+	{
 		Vectors[i] = CalculateVector(VectorsOrg[i]);
+		Vectors2D[i].x = (Vectors[i].x * 1024 / Vectors[i].z) / 8;
+		Vectors2D[i].y = (Vectors[i].y * 1024 / Vectors[i].z) / 8;
+	}
 }
 //////////////////////////////////////////////////////
 void	PrintVectors()
@@ -108,27 +117,27 @@ void	PrintVectors()
 	printf("--------\n");
 	printf("Vectors:\n");
 	printf("--------\n");
-	for(int i = 0;i < sizeof(VectorsOrg)/sizeof(VEC);i++)
-		printf("x: %i, y: %i, z: %i\n",Vectors[i].x,Vectors[i].y,Vectors[i].z);
+	for(int i = 0;i < sizeof(VectorsOrg)/sizeof(VEC3);i++)
+		printf("x: %i, y: %i, z: %i, 2D(%i,%i)\n",Vectors[i].x,Vectors[i].y,Vectors[i].z,Vectors2D[i].x,Vectors2D[i].y);
 }
 //////////////////////////////////////////////////////
-int	Dot(VEC v0,VEC v1)
+int	Dot(VEC3 v0,VEC3 v1)
 {
-	return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z;
+	return (v0.x * v1.x + v0.y * v1.y + v0.z * v1.z) >> 9;
 }
 //////////////////////////////////////////////////////
-VEC 	Cross(VEC v0,VEC v1)
+VEC3 	Cross(VEC3 v0,VEC3 v1)
 {
-	VEC v;
-	v.x = v0.y * v1.z - v0.z * v1.y;
-	v.y = v0.z * v1.x - v0.x * v1.z;
-	v.z = v0.x * v1.y - v0.y * v1.x;
+	VEC3 v;
+	v.x = (v0.y * v1.z - v0.z * v1.y) >> 9;
+	v.y = (v0.z * v1.x - v0.x * v1.z) >> 9;
+	v.z = (v0.x * v1.y - v0.y * v1.x) >> 9;
 	return v;
 }
 //////////////////////////////////////////////////////
-bool	IsPolygonVisible(int p,VEC source)
+bool	IsPolygonVisible(int p,VEC3 source)
 {
-	VEC v0,v1,v,s;
+	VEC3 v0,v1,v,s;
 	v0.x = Vectors[Polygons[p].vertices[0]].x - Vectors[Polygons[p].vertices[1]].x;
 	v0.y = Vectors[Polygons[p].vertices[0]].y - Vectors[Polygons[p].vertices[1]].y;
 	v0.z = Vectors[Polygons[p].vertices[0]].z - Vectors[Polygons[p].vertices[1]].z;
@@ -140,11 +149,12 @@ bool	IsPolygonVisible(int p,VEC source)
 	s.y = source.y - Vectors[Polygons[p].vertices[1]].y;
 	s.z = source.z - Vectors[Polygons[p].vertices[1]].z;
 	int	dot = Dot(v,s);
+	Polygons[p].dot = dot;
 	if(dot > 1) return true;
 	return false;
 }
 //////////////////////////////////////////////////////
-void	CheckPolygonsVisible(VEC source)
+void	CheckPolygonsVisible(VEC3 source)
 {
 
 	for(int i = 0;i < sizeof(Polygons)/sizeof(POLYGON);i++)
@@ -163,42 +173,51 @@ void	CheckPolygonsVisible(VEC source)
 		printf("Edge %i: %i\n",i,EdgeVisible[i]);
 }
 //////////////////////////////////////////////////////
-// cos(a) * cos(b) 			cos(c) * sin(b) + sin(c) * sin(a) * cos(b) 		sin(c) * sin(b) − cos(c) * sin(a) * cos(b)
-// −cos(a) * sin(b) 		cos(c) * cos(b) − sin(c) * sin(a) * sin(b) 		sin(c) * cos(b) + cos(c) * sin(a) * sin(b)
-// sin(a) 					−sin(c) * cos(a) 								cos(c) * cos(a)
+// cos(Y) * cos(Z) 			cos(X) * sin(Z) + sin(X) * sin(Y) * cos(Z) 		sin(X) * sin(Z) − cos(X) * sin(Y) * cos(Z)
+// −cos(Y) * sin(Z) 		cos(X) * cos(Z) − sin(X) * sin(Y) * sin(Z) 		sin(X) * cos(Z) + cos(X) * sin(Y) * sin(Z)
+// sin(Y) 					−sin(X) * cos(Y) 								cos(X) * cos(Y)
 
-// cAcB 					cCsB + sCsAcB 				sCcB − cCsAcB
+// cAcB 					cCsB + sCsAcB 				sCsB − cCsAcB
 // −cAsB			 		cCcB − sCsAsB 				sCcB + cCsAsB
 // sin(a) 					−sCcA			 			cCcA
 void	CalculateRotationMatrix()
 {
-	int cAcB = (Cos[WinkelA] * Cos[WinkelB]) >> 8;
-	int cAsB = (Cos[WinkelA] * Sin[WinkelB]) >> 8;
-	int sCsB = (Sin[WinkelC] * Sin[WinkelB]) >> 8;
-	int cCsB = (Cos[WinkelC] * Sin[WinkelB]) >> 8;
-	int cCcB = (Cos[WinkelC] * Cos[WinkelB]) >> 8;
-	int sCcB = (Sin[WinkelC] * Cos[WinkelB]) >> 8;
-	int sCcA = (Sin[WinkelC] * Cos[WinkelA]) >> 8;
-	int cCcA = (Cos[WinkelC] * Cos[WinkelA]) >> 8;
-	int sCsA = (Sin[WinkelC] * Sin[WinkelA]) >> 8;
-	int sCsAcB = (sCsA * Cos[WinkelB]) >> 8;
-	int sCsAsB = (sCsA * Sin[WinkelB]) >> 8;
-	int cCsA = (Cos[WinkelC] * Sin[WinkelA]) >> 8;
-	int cCsAcB = (cCsA * Cos[WinkelB]) >> 8;
-	int cCsAsB = (cCsA * Sin[WinkelB]) >> 8;
+	int c1 = Cos[WinkelY];
+	int c2 = Cos[WinkelX];
+	int c3 = Cos[WinkelZ];
+	int s1 = Sin[WinkelY];
+	int s2 = Sin[WinkelX];
+	int s3 = Sin[WinkelZ];
 
-	RotationMatrix[4 * 0 + 0] = cAcB;			RotationMatrix[4 * 0 + 1] = cCsB + sCsAcB;		RotationMatrix[4 * 0 + 2] = sCsB - cCsAcB;
-	RotationMatrix[4 * 1 + 0] = -cAsB;			RotationMatrix[4 * 1 + 1] = cCcB - sCsAsB;		RotationMatrix[4 * 1 + 2] = sCcB + cCsAsB;
-	RotationMatrix[4 * 2 + 0] = Sin[WinkelA];	RotationMatrix[4 * 2 + 1] = -sCcA;				RotationMatrix[4 * 2 + 2] = cCcA;
+	printf("CosY: %i\n",c1);
+	printf("CosX: %i\n",c2);
+	printf("CosZ: %i\n",c3);
+	printf("SinY: %i\n",s1);
+	printf("SinX: %i\n",s2);
+	printf("SinZ: %i\n",s3);
+
+	int *m = RotationMatrix;
+
+	m[4 * 0 + 0] = ((c1*c3)>>8) + ((((s1*s2)>>8)*s3)>>8);
+	m[4 * 0 + 1] = -((c1*s3)>>8) + ((((c3*s1)>>8)*s2)>>8);
+	m[4 * 0 + 2] = (c2*s1)>>8;
+
+	m[4 * 1 + 0] = (c2*s3)>>8;
+	m[4 * 1 + 1] = (c2*c3)>>8;
+	m[4 * 1 + 2] = -s2;
+	
+	m[4 * 2 + 0] = -((c3*s1)>>8) + ((((c1*s2)>>8)*s3)>>8);
+	m[4 * 2 + 1] = ((s1*s3)>>8) + ((((c1*c3)>>8)*s2)>>8);
+	m[4 * 2 + 2] = (c1*c2)>>8;
 
 	RotationMatrix[4 * 0 + 3] = 0;
 	RotationMatrix[4 * 1 + 3] = 0;
-	RotationMatrix[4 * 2 + 3] = 512;
+	RotationMatrix[4 * 2 + 3] = 1024;
 }
 //////////////////////////////////////////////////////
-VEC 	CalculateVector(VEC v)
+VEC3 	CalculateVector(VEC3 v)
 {
-	VEC r;
+	VEC3 r;
 	r.x = ((v.x * RotationMatrix[4 * 0 + 0] + v.y * RotationMatrix[4 * 0 + 1] + v.z * RotationMatrix[4 * 0 + 2]) >> 8) + RotationMatrix[4 * 0 + 3];
 	r.y = ((v.x * RotationMatrix[4 * 1 + 0] + v.y * RotationMatrix[4 * 1 + 1] + v.z * RotationMatrix[4 * 1 + 2]) >> 8) + RotationMatrix[4 * 1 + 3];
 	r.z = ((v.x * RotationMatrix[4 * 2 + 0] + v.y * RotationMatrix[4 * 2 + 1] + v.z * RotationMatrix[4 * 2 + 2]) >> 8) + RotationMatrix[4 * 2 + 3];
